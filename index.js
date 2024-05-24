10a72e8847109ac58d0b1b54b6e6e3b062b91c32
Dhruv P <169485603+dhruvp987@users.noreply.github.com>
2024-05-23 21:49:33 -0400

    Added proper touch support; added bounds to sliding; formatting

--- index.js
+++ index.js
@@ -1,7 +1,15 @@
 function moveBeadX(event, bead) {
     if (bead.hasPointerCapture(event.pointerId)) {
-        const beadRect = bead.getBoundingClientRect();
-        const offset = bead.parentElement.getBoundingClientRect().left + ((beadRect.right - beadRect.left) / 2);
-        bead.style.left = `${event.clientX - offset}px`;
+        const beadHalfWidth = bead.clientWidth / 2;
+        const offset = bead.parentElement.getBoundingClientRect().left + beadHalfWidth;
+        const relativePos = event.clientX - offset;
+        let rightBoundary;
+        if (relativePos < 0) {
+            bead.style.left = "0px";
+        } else if (relativePos > (rightBoundary = bead.parentElement.clientWidth - bead.clientWidth)) {
+            bead.style.left = rightBoundary - beadHalfWidth;
+        } else {
+            bead.style.left = `${relativePos}px`;
+        }
     }
 }
